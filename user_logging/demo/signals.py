8ae6199df358c4dedd37238f77118e874c4757d3
David Slusser <dslusser@box.com>
2020-10-18 17:45:10 -0700

    adding user_logging topic code

---
+++ user_logging/demo/signals.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.signals import user_logged_in, user_logged_out, user_login_failed
+from django.dispatch import receiver
+import logging
+
+# import models
+from django.contrib.auth.models import User
+
+
+user_logger = logging.getLogger('user')
+
+
+# log user login succeeded
+@receiver(user_logged_in)
+def log_user_login(sender, user, **kwargs):
+    """ log user login to user log """
+    user_logger.info(f'login successful for user: {user}')
+
+
+# log user login failed
+@receiver(user_login_failed)
+def log_user_login_failed(sender, user=None, **kwargs):
+    """ log user login to user log """
+    login_username = kwargs['credentials'].get('username', None)
+    if not user and login_username:
+        user = User.objects.filter(username=login_username).last()
+    if user:
+        user_logger.info(f'login failed for user: {user}')
+    else:
+        user_logger.info('login failed; unknown user')
+
+
+# log user logout
+@receiver(user_logged_out)
+def log_user_logout(sender, user, **kwargs):
+    """ log user logout to user log """
+    user_logger.info(f'logout successful for user: {user}')
